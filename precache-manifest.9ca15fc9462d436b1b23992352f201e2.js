141b1bbaf28ae8427c67bbea458cb9ba843f6762
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-10-31 10:28:01 +0200

    Updates

---
+++ precache-manifest.9ca15fc9462d436b1b23992352f201e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf3d879a4b6118bb501b55247874b69",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "9d7ba74f65af861c0a2f",
+    "url": "/react_add-todo-form/static/css/main.358660eb.chunk.css"
+  },
+  {
+    "revision": "848921d38395d702d42c",
+    "url": "/react_add-todo-form/static/js/2.cad89506.chunk.js"
+  },
+  {
+    "revision": "9d7ba74f65af861c0a2f",
+    "url": "/react_add-todo-form/static/js/main.26cf2232.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
